9e5d4280ff0708c5f49757839e898682b9245843
RomanSemenskiy <st142067@student.spbu.ru>
2025-11-07 17:08:27 +0300

    Добавил утилиту + немного подкорректировал названия тестов

--- src/bin_search.py
+++ src/bin_search.py
@@ -1,5 +1,16 @@
 def bin_search(xs: list[int], x: int):
+
+    if not isinstance(xs, list) or not xs:
+        return -1
+
+    if not isinstance(x, int):
+        return -1
+
+    for i in xs:
+        if not isinstance(i, int):
+            return -1
     left, right = 0, len(xs) - 1
+
     while left <= right:
         mid = (left + right) // 2
         if xs[mid] == x:
--- src/checksum.py
+++ src/checksum.py
@@ -1,20 +1,34 @@
 def modulo11_checksum(isbn_number: str):
 
+    if not isinstance(isbn_number, str):
+        return False
+
     digits = [int(char) for char in isbn_number if char.isdigit()]
 
     if len(digits) != 10:
-	return False
+        return False
 
     check_digit = digits[-1]
-
     total = 0
-    
+
     for i in range(len(digits) - 1):
         weight = 10 - i
         digit = digits[i]
         total += digit * weight
 
     total += check_digit
-    
     return total % 11 == 0
 
+print("Проверка ISBN-10, чтобы вйти напишите -1")
+
+while True:
+    isbn = input("Введите строку: ")
+    
+    if isbn == "-1":
+        print("Выход")
+        break
+    
+    if modulo11_checksum(isbn):
+        print("correct")
+    else:
+        print("incorrect")
