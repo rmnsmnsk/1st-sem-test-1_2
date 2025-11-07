205b7033c8b6efca2494188e815260a4f893d1ce
RomanSemenskiy <st142067@student.spbu.ru>
2025-11-07 16:34:13 +0300

    Добавил тесты, в т.ч те, которые проверяют дополнения

--- test/test_bin_searh.py
+++ test/test_bin_searh.py
@@ -1,13 +1,26 @@
-from src.bin_search import binSearch
+from src.bin_search import bin_search
 
 
 def test_middle():
-    assert binSearch([1, 2, 3, 4, 5], 4) == 3
+    assert bin_search([1, 2, 3, 4, 5], 4) == 3
 
 
 def test_start():
-    assert binSearch([1, 2, 3, 4], 2) == 1
+    assert bin_search([1, 2, 3, 4], 2) == 1
 
 
 def test_not_in_list():
-    assert binSearch([1, 2, 3, 4], 5) == -1
+    assert bin_search([1, 2, 3, 4], 5) == -1
+
+def test_single():
+    assert bin_search([6], 6) == 0
+    assert bin_search([2], 3) == -1
+
+def empty():
+    assert bin_search([], 8) == -1
+
+def first():
+    assert bin_search([5, 2, 3, 4], 5) == 0
+
+def last():
+    assert bin_search([1, 2, 3, 8], 8) == 3
--- test/test_checksum.py
+++ test/test_checksum.py
@@ -1,9 +1,16 @@
-from src.checksum import modulo11Checksum
+from src.checksum import modulo11_checksum
 
 
 def test_good():
-    assert modulo11Checksum("2-266-11156-8")
+    assert modulo11_checksum("2-266-11156-6")
 
+def test_only_digits():
+    assert modulo11_checksum("2266111566")
+
+def test_wrong_length():
+    assert not modulo11_checksum("12323")
+    assert not modulo11_checksum("1234123215678901234")
 
 def test_bad():
-    assert not modulo11Checksum("2-266-11156-3")
+    assert not modulo11_checksum("2-266-11156-3")
+
